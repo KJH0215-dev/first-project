ec7280893922101bf060e0c6cc4371f7c99a711a
KJH0215 <kimjihan0083@gmail.com>
2026-03-04 10:49:45 +0900

    퍼센트_몰 변환 계산기 제작

--- python-project.py
+++ python-project.py
@@ -1,5 +1,10 @@
-user_input =input("값을 넣으시오:")
-if user_input == "a":
-    print(10)
-else:
-    print("a가 아니에요")
+percent = float(input("질량 퍼센트를 넣으세요:"))
+density = float(input("밀도를 넣으세요:"))
+molarmass = float(input("몰질량을 넣으세요:"))
+def 퍼센트_몰_변환(percent, density, molarmass):
+    molarity = (10*percent*density)/molarmass
+    return molarity
+result = 퍼센트_몰_변환(percent, density, molarmass)
+print(f"M(몰농도)={result:.2f}M")
+
+
